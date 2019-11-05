c6b074e588850f0440b600356518bb578c60f729
Jayalakshmi RM <jayalakshmi.rm@anthem.com>
2019-11-05 06:11:48 -0500

    Updated route-service.service.ts

--- src/app/services/routes/route-service.service.ts
+++ src/app/services/routes/route-service.service.ts
@@ -1,34 +1,31 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, Route, ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { LoginComponent } from 'src/app/components/login/login.component';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
+import {map, catchError, tap} from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 import { ProductDetailsComponent } from 'src/app/components/product-details/product-details.component';
 import { HttpServiceService } from '../http-service/http-service.service';
 import { ProductService } from '../products/products.service';
 
 @Injectable()
 export class  RouteServiceService  implements CanActivate {
-product:ProductDetailsComponent
- productDetails; 
+product: ProductDetailsComponent;
+ productDetails;
 productID;
-  constructor(private activatedRoute:ActivatedRoute,private httpservice:HttpServiceService, private _router: Router,private productservice:ProductService ) {
+  constructor(private httpservice: HttpServiceService, private productservice: ProductService ) {
   }
 
-  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> | Promise<any> | any {
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
    // let temp=this.httpservice.getService('product/productlist').;
-   console.log(next.params);
+   console.log(next);
    this.productID = next.params.productID;
-     this.httpservice.getService('product/productdetails/' + this.productID).subscribe(
+   return this.httpservice.getService('product/productdetails/' + this.productID).pipe(map(
       (res) => {
-       this.productservice.setProducts(res['product']);
-        //this._router.navigate(['/productdetails']);
-        return true;
-      },
-      
-    );
-   
-  return true;
-
+        if (res && res.product) {
+       this.productservice.setProducts(res.product);
+       return true;
+      }
+      }),
+      catchError((er: any, caught: Observable<any>) => of(false)));
   }
 
 }
